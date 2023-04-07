398131f0b807fe174274df6fbbe2c3f340864f93
Neel Popat <neelpopat242@gmail.com>
2023-04-07 22:29:49 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require('dotenv').config({path:"./etc/secrets/.env"})
-console.log(process.env.DB_URL, "URL in db.js")
+console.log(process.env.DB_HOST, "URL in db.js")
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(process.env.DB_HOST);
     console.log("Connected to mongoDB.");
   } catch (error) {
     throw error;
