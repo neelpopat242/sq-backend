eb0e01215c0c18d9a91fd9ec49046d4d409607a3
Neel Popat <neelpopat242@gmail.com>
2023-04-07 22:41:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 
 const app = require ("./app")
 const connectDatabase = require("./config/database")
-const dotenv = require("dotenv")
-dotenv.config({path:"./etc/secrets/.env"})
+require("dotenv").config({path:".env"})
 console.log(process.env.DB_URL)
 
 app.get('/', function(req, res){
